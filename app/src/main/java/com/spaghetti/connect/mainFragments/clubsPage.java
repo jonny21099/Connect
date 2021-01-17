ec1c6a8490ba5cfd5ab98c7c7d4a4fd9ffda8ee1
jonny21099 <jcmartin@ualberta.ca>
2021-01-16 19:04:36 -0700

    Added recyclerViewAdapter for clubs

--- app/src/main/java/com/spaghetti/connect/mainFragments/clubsPage.java
+++ app/src/main/java/com/spaghetti/connect/mainFragments/clubsPage.java
@@ -1,9 +1,12 @@
 package com.spaghetti.connect.mainFragments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,59 +14,88 @@ import android.view.ViewGroup;
 
 import com.spaghetti.connect.ClubsPageActivity;
 import com.spaghetti.connect.R;
+import com.spaghetti.connect.data.Club;
+import com.spaghetti.connect.data.Post;
+import com.spaghetti.connect.ui.recyclerViewAdapter.BookmarkRCA;
+import com.spaghetti.connect.ui.recyclerViewAdapter.ClubsRCA;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link clubsPage#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class clubsPage extends Fragment {
 
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    public clubsPage() {
-//        // Required empty public constructor0
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment page3.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static clubsPage newInstance(String param1, String param2) {
-//        clubsPage fragment = new clubsPage();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
+public class clubsPage extends Fragment {
+    private View view;
+    private Context c;
+
+    RecyclerView clubsRcView;
+    RecyclerView.Adapter clubsAdapter;
+    RecyclerView.LayoutManager clubsLayoutManager;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public clubsPage() {
+        // Required empty public constructor0
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment page3.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static clubsPage newInstance(String param1, String param2) {
+        clubsPage fragment = new clubsPage();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Club club = new Club("UaoC", "something@ualberta.ca", "this is a club to");
+        ArrayList<Club> clubList = new ArrayList<>();
+
+        clubList.add(club);
+        clubList.add(club);
+
+
         // Inflate the layout for this fragment
-        Intent intent = new Intent(getContext(), ClubsPageActivity.class);
-        return inflater.inflate(R.layout.fragment_clubspage, container, false);
+        view = inflater.inflate(R.layout.fragment_clubspage, container, false);
+
+        clubsRcView = view.findViewById(R.id.clubRecyclerView);
+        clubsLayoutManager = new LinearLayoutManager(c);
+        clubsRcView.setLayoutManager(clubsLayoutManager);
+
+        clubsAdapter = new ClubsRCA(clubList);
+        clubsRcView.setAdapter(clubsAdapter);
+
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/spaghetti/connect/ui/recyclerViewAdapter/ClubsRCA.java
@@ -0,0 +1,66 @@
+package com.spaghetti.connect.ui.recyclerViewAdapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.spaghetti.connect.R;
+import com.spaghetti.connect.data.Club;
+
+import java.util.ArrayList;
+
+public class ClubsRCA extends RecyclerView.Adapter<ClubsRCA.PostViewHolder> {
+
+    ArrayList<Club> data;
+
+    public static class PostViewHolder extends RecyclerView.ViewHolder {
+        ConstraintLayout layout;
+        ImageView image;
+        TextView description;
+        TextView name;
+
+        PostViewHolder(View requestView) {
+            super(requestView);
+            layout = requestView.findViewById(R.id.postViewHolderLayout);
+            image = requestView.findViewById(R.id.postViewHolderImage);
+            name = requestView.findViewById(R.id.postViewHolderTitle);
+            description = requestView.findViewById(R.id.postViewHolderContent);
+        }
+    }
+
+    public ClubsRCA(ArrayList<Club> data) {
+        this.data = data;
+    }
+
+    @Override
+    public ClubsRCA.PostViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.postviewholder, parent, false);
+        return new ClubsRCA.PostViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ClubsRCA.PostViewHolder holder, int position) {
+        Club c = data.get(position);
+
+        holder.name.setText(c.getName());
+        holder.image.setImageBitmap(c.getImage());
+        holder.description.setText(c.getDescription());
+
+        holder.layout.setOnClickListener(v -> {
+            // TODO
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+}
+
